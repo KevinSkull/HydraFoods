5ef67a5a31431effd09a761135245dc3c589809c
Kevin <kevinalphau@gmail.com>
2021-02-18 17:47:07 +0000

    The basic template for a ordering system

---
+++ CLI.py
@@ -0,0 +1,36 @@
+# Command Line interface test setup for the backend
+
+while True:
+    try:
+
+            choice = int(input("Pick a choice between foods\n"
+                               "1. Chips\n"
+                               "2. Fish\n"
+                               "3. Chicken\n"
+                               "4. Burgers\n"
+                               ": "))
+
+            if choice == 1:
+                print("Placeholder for SQL code")
+            elif choice == 2:
+                print("Placeholder for SQL code")
+            elif choice == 3:
+                print("Placeholder for SQL code")
+            elif choice == 4:
+                print("Placeholder for SQL code")
+            elif choice == 5:
+                exit()
+
+
+
+    except ValueError:
+        print("That was not a valid number, please try ordering again")
+
+
+
+
+
+
+
+
+
