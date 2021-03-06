782c8b4eb636aa490905ff35d93165dfb2690bce
Kevin <kevinalphau@gmail.com>
2021-03-06 17:14:58 +0000

    Added SQL input into the database

--- CLI.py
+++ CLI.py
@@ -1,31 +1,54 @@
 # Command Line interface test setup for the backend to add information to the database
 from MySQLconnector import *
 
-while True:
+
+def insert_order_chips(full_nameS, amountI, addressS):
     try:
-
-            choice = int(input("Pick a choice between foods\n"
-                               "1. Chips\n"
-                               "2. Fish\n"
-                               "3. Chicken\n"
-                               "4. Burgers\n"
-                               ": "))
-
-            if choice == 1:
-                print("Placeholder for SQL code")
-            elif choice == 2:
-                print("Placeholder for SQL code")
-            elif choice == 3:
-                print("Placeholder for SQL code")
-            elif choice == 4:
-                print("Placeholder for SQL code")
-            elif choice == 5:
-                exit()
+        records_list = [full_nameS, amountI, addressS]
+        attributes_list = ['full_name', 'amount', 'address']
+        formatted_record_list = str(records_list).replace("[", "").replace("]", "")
+        formatted_atk_list = str(attributes_list).replace("'", "").replace("[", "").replace("]", "")
+        one_row = "INSERT INTO {} ({}) VALUES ({})".format("chips_ordering", formatted_atk_list, formatted_record_list)
+        commands.execute(one_row)
+        db_setup.commit()
+        print("Record(s) have been inserted successfully")
+        input("Press Enter")
+    except:
+        print("You have entered something wrong")
+        input("Press Enter")
 
 
 
-    except ValueError:
-        print("That was not a valid number, please try ordering again")
+
+while True:
+
+    choice = int(input("Pick a choice between foods\n"
+                           "1. Chips\n"
+                           "2. Fish\n"
+                           "3. Chicken\n"
+                           "4. Burgers\n"
+                           ": "))
+
+    if choice == 1:
+        fullname = str(input("Please enter your full name: "))
+        amount = int(input("Please enter the amount you wish to order: "))
+        address = str(input("Please enter your full address: "))
+        insert_order_chips(fullname, amount, address)
+        exit()
+
+    elif choice == 2:
+        print("Placeholder for SQL code")
+    elif choice == 3:
+        print("Placeholder for SQL code")
+    elif choice == 4:
+        print("Placeholder for SQL code")
+    elif choice == 5:
+        exit()
+
+
+
+
+
 
 
 
