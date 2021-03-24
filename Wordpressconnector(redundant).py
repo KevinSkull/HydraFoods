0ff2731d6125d788ad4a270f50cc403339bb4750
Kevin <kevinalphau@gmail.com>
2021-03-24 14:57:15 +0000

    Code found not to be useful.

---
+++ Wordpressconnector(redundant).py
@@ -0,0 +1,15 @@
+
+
+
+#import cdata.wordpress as mod
+#user = input("Username: ")
+#password = input("Pass: ")
+
+#connection = mod.connect("URL=http://localhost/testsite/wp-admin/;User=;Password=;InitiateOAuth=GETANDREFRESH;OAuthClientId=1;OAuthClientSecret=QQB2sKjzLOmOAO8uK25e8eF2kimI3KTadB53BadufDl9LgoG;CallbackURL=http://localhost:portnumber;")
+#connection = mod.connect("URL=http://localhost/testsite/;User={};Password={};".format(user, password))
+#wp_cursor = connection.cursor()
+#print(wp_cursor)
+#wp_cursor.execute("SELECT ID, Name FROM wp_users")
+#rs = wp_cursor.fetchall()
+#for row in rs:
+ #   print(row)
